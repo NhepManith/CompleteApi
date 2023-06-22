1e83513d5b8cc35f33a673986a9478ebd844d86d
Manith Nhep <nmanith@cellcard.com.kh>
2023-06-22 14:29:17 +0700

    FirstCommit

---
+++ BookApi/Controllers/BooksController.cs
@@ -0,0 +1,81 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static List<Book> _books = new List<Book>() {
+
+        new Book()
+        
+        };
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetAllBooks()
+        {
+            return _books;
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Book> GetBookById(int id)
+        {
+            var book = _books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+
+            }
+
+            return book;
+
+        }
+
+        [HttpPost]
+
+        public ActionResult<Book> AddBook(Book book)
+        {
+            book.Id = _books.Count + 1;
+            _books.Add(book);
+
+            return CreatedAtAction(nameof(GetBookById), new {id=book.Id} , book);
+
+        }
+
+        [HttpPut("{id}")]
+
+        public ActionResult <Book> UpdateBook (int id, Book UpdateBook)
+        {
+            var book = _books.FirstOrDefault(b =>b.Id == id);
+            if (book == null)
+                return NotFound();
+
+            book.Title = UpdateBook.Title;
+            book.Author = UpdateBook.Author;
+            book.Year = UpdateBook.Year;
+
+            return NoContent();
+        }
+
+        [HttpDelete ("{id}")]
+
+        public ActionResult<Book> DeleteBook(int id)
+        {
+            var book = _books.FirstOrDefault(a => a.Id == id);
+            
+            if(book == null)
+                return NotFound();
+
+            _books.Remove(book);
+
+            return book;
+                
+        }
+
+    }
+}
---
+++ BookApi/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BookApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; } = 0;
+
+        public string Title { get; set; } = "Education";
+
+        public string Author { get; set; } = "Manith";
+
+        public int Year { get; set; } = DateTime.Now.Year;
+
+      
+    }
+
+
+}
